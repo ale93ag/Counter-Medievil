d975a5a1a789df10c74459532922b775a0261984
ale93ag <alessandragalotta93@yahoo.com>
2023-07-18 18:19:30 +0200

    Creato bottone per impostare il valore di incremento

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,21 +1,33 @@
 let contatore = 0;
+let valoreImpostato = 0;
+
+function impostaValore() {
+    const incrementoInput = document.getElementById("incrementoInput");
+    valoreImpostato = parseInt(incrementoInput.value);
+
+    if (isNaN(valoreImpostato)) {
+        return; // Esci dalla funzione se l'input non è un numero valido
+    }
+}
 
 function aggiungi() {
-    contatore++;
+    contatore += valoreImpostato;
     document.getElementsByClassName("count")[0].innerHTML = contatore;
+
     if (contatore >= 0) {
         document.getElementsByClassName("count")[0].style.color = ""; // Reimposta il colore predefinito
     }
 }
 
 function diminuisci() {
-    contatore--;
+    contatore -= valoreImpostato;
     document.getElementsByClassName("count")[0].innerHTML = contatore;
 
     if (contatore < 0) {
         document.getElementsByClassName("count")[0].style.color = "red";
-    } 
+    }
 }
+
 
 function reset() {
     if (contatore !== 0) {
