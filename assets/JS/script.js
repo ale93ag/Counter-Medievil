ee2b829a8918debbc00688be4a1c44ddc9a77896
ale93ag <alessandragalotta93@yahoo.com>
2023-07-20 16:51:27 +0200

    Sono riuscita a scrivere tutto JavaSript, adesso devo collegare css

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,108 +1,5 @@
-
-
- // Creazione e aggiunta degli elementi con JavaScript
-
- const mainContainer = document.querySelector('main');
- mainContainer.setAttribute('class', 'contenitore');
-
- // Creazione del div per il reset
- const resetContainer = document.createElement('div');
- resetContainer.setAttribute('class','contenitore-reset');
- mainContainer.appendChild(resetContainer);
-
- // Creazione del bottone di reset
- const resetButton = document.createElement('button');
- resetButton.textContent = 'Reset';
- resetButton.setAttribute('class', 'button-reset');
- document.querySelector('.button-reset').addEventListener('click', reset);
- resetContainer.appendChild(resetButton);
-
- // Creazione del font awesome icon per il reset
- const resetIcon = document.createElement('i');
- resetIcon.setAttribute('class', 'fas', 'fa-undo');
- resetButton.prepend(resetIcon);
-
- // Creazione dello span per il conteggio
- const countSpan = document.createElement('span');
- countSpan.textContent = '0';
- countSpan.setAttribute('class', 'count');
- mainContainer.appendChild(countSpan);
-
- // Creazione del div per il decremento
- const lessContainer = document.createElement('div');
- lessContainer.setAttribute('class', 'contenitore-meno');
- mainContainer.appendChild(lessContainer);
-
- // Creazione del bottone di decremento
- const lessButton = document.createElement('button');
- lessButton.setAttribute('class', 'button-less');
- document.querySelector('.button-less').addEventListener('click', diminuisci);
- lessContainer.appendChild(lessButton);
-
- // Creazione dello span per il segno meno
- const lessSpan = document.createElement('span');
- lessSpan.textContent = '-';
- lessSpan.setAttribute('class', 'sign');
- lessButton.appendChild(lessSpan);
-
- // Creazione dell'immagine di una bara per il decremento
- const lessImage = document.createElement('img');
- lessImage.src = 'assets/Icone/coffin.png';
- lessImage.alt = 'immagine di una bara';
- lessImage.setAttribute('class', 'bara');
- lessButton.appendChild(lessImage);
-
- // Creazione del div per l'incremento
- const plusContainer = document.createElement('div');
- plusContainer.setAttribute('class', 'contenitore-più');
- mainContainer.appendChild(plusContainer);
-
- // Creazione del bottone di incremento
- const plusButton = document.createElement('button');
- plusButton.setAttribute('class', 'button-plus');
- document.querySelector('.button-plus').addEventListener('click', aggiungi);
- plusContainer.appendChild(plusButton);
-
- // Creazione dello span per il segno più
- const plusSpan = document.createElement('span');
- plusSpan.textContent = '+';
- plusSpan.setAttribute('class', 'sign');
- plusButton.appendChild(plusSpan);
-
- // Creazione dell'immagine di una bara per l'incremento
- const plusImage = document.createElement('img');
- plusImage.src = 'assets/Icone/coffin.png';
- plusImage.alt = 'immagine di una bara';
- plusImage.setAttribute('class', 'bara');
- plusButton.appendChild(plusImage);
-
- // Creazione del div per l'impostazione del valore
- const incrementoContainer = document.createElement('div');
- incrementoContainer.setAttribute('class', 'incremento');
- mainContainer.appendChild(incrementoContainer);
-
- // Creazione del label per l'input di impostazione del valore
- const incrementoLabel = document.createElement('label');
- incrementoLabel.textContent = 'Imposta valore:';
- incrementoLabel.setAttribute('for', 'incrementoInput');
- incrementoContainer.appendChild(incrementoLabel);
-
- // Creazione dell'input per l'impostazione del valore
- const incrementoInput = document.createElement('input');
- incrementoInput.type = 'number';
- incrementoInput.id = 'incrementoInput';
- incrementoInput.value = '1';
- incrementoContainer.appendChild(incrementoInput);
-
- // Creazione del bottone per l'impostazione del valore
- const impostazioneButton = document.createElement('button');
- impostazioneButton.textContent = 'Imposta';
- document.querySelector('button').addEventListener('click', impostaValore);
-;
- incrementoContainer.appendChild(impostazioneButton);
- 
 let contatore = 0;
-
+let valoreImpostato = 1;
 
 function impostaValore() {
     const incrementoInput = document.getElementById("incrementoInput");
@@ -140,6 +37,126 @@ function reset() {
     document.getElementsByClassName("count")[0].innerHTML = contatore;
     document.getElementsByClassName("count")[0].style.color = ""; // Reimposta il colore predefinito
 }
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Il tuo codice qui
+  
+//Creo l'header contenitore
+const header = document.createElement("header");
+header.setAttribute('class', 'header');
+
+
+//creo il figlio h1 di header
+const h1 = document.createElement("h1");
+h1.textContent = "Counter di Alessandra"
+header.append(h1);
+document.body.appendChild(header);
+
+
+//creo il main contenitore
+ const main = document.createElement("main")
+ main.setAttribute('class', 'contenitore');
+ document.body.appendChild(main);
+
+ //creo il div che contiene il Reset
+ const resetDiv = document.createElement("div");
+ resetDiv.setAttribute('class', 'contenitore-reset');
+ main.appendChild(resetDiv);
+
+ //creo il button per il reset
+ const resetButton = document.createElement("button");
+ resetButton.textContent = "Reset";
+ resetButton.setAttribute('class', 'button-reset')
+ resetDiv.appendChild(resetButton);
+ resetButton.addEventListener('click', reset);
+
+    // creo l'icona di reset usando Font Awesome
+    const resetIcon = document.createElement('i');
+    resetIcon.classList.add('fas', 'fa-undo');
+    resetButton.prepend(resetIcon);
+
+ //creo lo span del contatore 
+ const spanCounter =  document.createElement("span");
+ spanCounter.textContent = '0';
+ spanCounter.setAttribute('class', 'count');
+ main.appendChild(spanCounter);
+
+ //creo il contenitore per il pulsante meno
+const lessDiv = document.createElement('div');
+lessDiv.setAttribute('class', 'contenitore-meno');
+main.appendChild(lessDiv);
+
+//creo il button diminuisci
+const buttonLess = document.createElement('button');
+buttonLess.setAttribute('class', 'button-less');
+lessDiv.appendChild(buttonLess);
+buttonLess.addEventListener('click', diminuisci);
+
+// creo lo span del segno -
+const spanMeno = document.createElement('span');
+spanMeno.setAttribute('class', 'sign');
+spanMeno.textContent = '-';
+buttonLess.appendChild(spanMeno);
+
+//creo l'immagine della bara 
+const imgBara = document.createElement('img');
+imgBara.setAttribute('class', 'bara');
+imgBara.src = 'assets/Icone/coffin.png';
+imgBara.alt = 'immagine di una bara';
+buttonLess.appendChild(imgBara);
+
+
+ //creo div contenitore +
+ const plusDiv = document.createElement('div');
+ plusDiv.setAttribute('class', 'contenitore-più');
+ main.appendChild(plusDiv);
+
+ //creo il button del +
+const buttonPlus = document.createElement('button');
+buttonPlus.setAttribute('class', 'button-plus');
+plusDiv.appendChild(buttonPlus);
+buttonPlus.addEventListener('click', aggiungi);
+
+// creo lo span del segno -
+const span = document.createElement('span');
+span.setAttribute('class', 'sign');
+span.textContent = '+',
+buttonPlus.appendChild(span);
+
+ //creo l'immagine della bara 
+ const img = document.createElement('img');
+ img.setAttribute('class', 'bara');
+ img.src = 'assets/Icone/coffin.png';
+ img.alt = 'immagine di una bara';
+ buttonPlus.appendChild(img);
+
+//creo il div del contenitore incremento
+const divIncremento = document.createElement('div');
+divIncremento.setAttribute('class', 'incremento');
+main.appendChild(divIncremento);
+
+//creo l'input per inserire il valore
+const input = document.createElement('input');
+input.setAttribute('id', 'incrementoInput');
+input.setAttribute('type', 'number');
+input.value = '1';
+divIncremento.appendChild(input);
+
+//creo il label dell'input
+const label = document.createElement('label');
+label.setAttribute('for', 'incrementoInput');
+label.textContent = 'Imposta valore:'
+divIncremento.appendChild(label);
+
+const buttonImposta = document.createElement('button');
+buttonImposta.setAttribute('class', 'impostaValoreIncremento');
+buttonImposta.textContent = 'Imposta';
+buttonImposta.addEventListener('click', impostaValore);
+// Aggiungi il pulsante "Imposta" al contenitore "divIncremento"
+divIncremento.appendChild(buttonImposta);
+});
 
 
 
